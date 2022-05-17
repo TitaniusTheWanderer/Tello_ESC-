92f2ce46448833f5d5583e964e181626ba203de0
TitaniusTheWanderer <105313049+TitaniusTheWanderer@users.noreply.github.com>
2022-05-17 10:16:30 -0700

    Create drone code.py

---
+++ drone code.py
@@ -0,0 +1,48 @@
+from djitellopy import tello
+from time import sleep
+
+
+tello = tello.Tello
+
+tello.connect()
+
+# go get battery
+
+print(tello.get_battery())
+
+#take off
+
+tello.takeoff()
+
+#move to correct high
+
+tello.move_up(100)
+
+#move forward
+
+tello.move_forward(700)
+
+#turn left(counter clockwise)
+
+tello.rotate_counter_clockwise(90)
+
+#move forward(again yeah)
+
+
+tello.move_forward(350)
+
+#we do a little reverse
+
+tello.move_back(350)
+
+#turn right(clockwise)
+
+tello.rotate_clockwise(90)
+
+#a weee bit more turning
+
+tello.move_back(700)
+
+#and done
+
+tello.land
